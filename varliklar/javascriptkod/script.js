005f925bf8a056382d7d959a092b807417098c1d
Fatih Mutaf <fmutaf10@gmail.com>
2026-03-24 13:56:53 +0300

    ilk depoya dönüş

--- varliklar/javascriptkod/script.js
+++ varliklar/javascriptkod/script.js
@@ -2,5 +2,17 @@ function kareAl() {
     let sonuc = sayi*sayi;
     console.log(sonuc);
 }
+function kareAl() {
+    let sonuc = sayi*sayi;
+    console.log(sonuc);
+}
+function kareAl() {
+    let sonuc = sayi*sayi;
+    console.log(sonuc);
+}
+function kareAl() {
+    let sonuc = sayi*sayi;
+    console.log(sonuc);
+}
 
 
